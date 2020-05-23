b836804a9d52e684b8f0c178042a0c8d09e11458
omarelia <omarcho07@hotmail.com>
2020-05-22 23:44:32 -0300

    A lot of errors, but my brain is burned

--- src/generate.py
+++ src/generate.py
@@ -5,18 +5,50 @@ import logging
 import logging.config
 import pyodbc
 
+### Si utilizo esta configuración, logro que se grabe el archivo de logs.
+### Pero, aunque el programa finalize, el archivo '', sigue actualizandose continuamente
+### cada minuto aprox.!!!; aunque lo haga abortar con 'ctrl + C'
+### Me sale un "File doesn't exist", durante la corrida, repetidamente...
+### Pero con la cabeza quemada, no le encuentro explicación. (ya no lo hace)
 
 logging.basicConfig(filename='/dist/logs/generate.log', level=logging.DEBUG,
                     format='%(asctime)s : %(levelname)s : %(message)s', 
-                    datefmt='%d/%m/%Y %I:%M:%S', 
+                    datefmt='%a, %d %b %Y %H:%M:%S', 
                     filemode='w')
 
-logging.info('Hola')
+
+### Si trato de utilizar esta configuración, los logs salen por terminal!
+### Va a 'logging.conf', pero obviamente está mal definido
+### Busque mucho, pero no encuentro el problema...
+### lo de agregarle el filename en cada linea, no es una forma linda; pero quería asegurarme
+
+#logging.config.fileConfig('logging.conf')
+
+logger=logging.getLogger(__name__)
 
 file=open('salida.txt','r')
+logger.debug('Abro el archivo salida.txt')
 
+### Pasé estos parámetros a .env
 '''
-CREATE TABLE IF NOT EXISTS salida(
+serverName='localhost'
+dbName='Prueba'
+user='sa'
+psw='Pass@word'
+'''
+
+logger.info('Inicio')
+
+try:
+    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+                              serverName+';DATABASE='+dbName+';UID='+user+';PWD=' + psw)
+    logging.debug('Conexión exitosa a SQL Server')
+except Exception as e:
+    logging.debug('Ocurrió un error al conectar a SQL Server: ' + str(e))
+#    exit()
+
+
+'''CREATE TABLE IF NOT EXISTS salida(
 	id bigint identity(1,1) primary key,	
     legajo INT(5) NOT NULL,	
 	apellidoNombre VARCHAR(255) NOT NULL,
@@ -26,24 +58,12 @@ CREATE TABLE IF NOT EXISTS salida(
 	nueSueld VARCHAR(255) NOT NULL,
 	difMinjus INT(9) NOT NULL,
 	difSueld INT(9) NOT NULL,
-);
-'''
+);'''
 
-serverName='localhost'
-dbName='Prueba'
-user='sa'
-psw='Pass@word'
-
-
-try:
-    conexion = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
-                              serverName+';DATABASE='+dbName+';UID='+user+';PWD=' + psw)
-    logging.debug('Conexión exitosa a SQL Server')
-except Exception as e:
-    logging.debug('Ocurrió un error al conectar a SQL Server: ' + str(e))
 
 for line in file:
-    logging.debug('El registro se procesó correctamente')
+    row = line
+    logger.debug('El registro se procesó correctamente: ' + row)
 
 
-logging.info('Hasta luego')
+logger.info('Salida')
